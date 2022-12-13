b454f42a4ee48a9137c20586908ad6b177f3a2e2
UroobaShameem <uroobashameem12@gmail.com>
2022-12-13 22:54:53 +0500

    updated

--- createnode.h
+++
@@ -1,7 +0,0 @@
-#ifndef CREATE_NODE_H
-#define CREATE_NODE_H
-
-struct node;
-node *createnode();
-
-#endif
--- main.cpp
+++ main.cpp
@@ -6,9 +6,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-//#include "main.h"
-//#include "search.h"
-
 using namespace std;
 
 typedef  struct node
